bc0f8225173bf237433f31ad887a132f3574dd9c
joko0811 <okano@sec.inf.shizuoka.ac.jp>
2022-11-20 22:39:15 +0900

    勾配が取れないので試行錯誤中

--- SSD-background-patches/loss.py
+++ SSD-background-patches/loss.py
@@ -3,6 +3,72 @@ import torch
 from model.yolo_util import detections_ground_truth, detections_loss
 
 
+class BPLoss(torch.nn.Module):
+    def __init__(self):
+        super(BPLoss, self).__init__()
+
+    def forward(self, detections: detections_loss, ground_truthes: detections_ground_truth):
+
+        tpc_score = self.tpc(detections, ground_truthes)
+        tps_score = self.tps(detections, ground_truthes)
+        fpc_score = self.fpc(detections, ground_truthes)
+
+        loss = tpc_score+tps_score+fpc_score
+        return loss
+
+    def tpc(self, detections: detections_loss, ground_truthes: detections_ground_truth):
+        """True Positive Class Loss
+        """
+        tpc_score = torch.tensor([0], dtype=torch.float,
+                                 device=detections.data.device)
+
+        gt_labels_for_nearest_dt = ground_truthes.class_labels[detections.nearest_gt_idx]
+        calculated_class_score = detections.class_scores.detach()
+
+        for i in range(calculated_class_score.shape[0]):
+            ignore_idx = gt_labels_for_nearest_dt[i]
+            calculated_class_score[i, ignore_idx] = 0
+
+            tpc_score[0] += (detections.z[i] *
+                             torch.log(torch.max(calculated_class_score[i])))
+
+        tpc_score *= -1
+        return tpc_score
+
+    def tps(self, detections: detections_loss, ground_truthes: detections_ground_truth):
+        """True Positive Shape Loss
+        """
+        tps_score = torch.tensor([0], dtype=torch.float,
+                                 device=detections.data.device)
+
+        # (x-x)^2+(y-y)^2+(w-w)^2+(h-h)
+        dist = (
+            (detections.xywh-ground_truthes.xywh[detections.nearest_gt_idx])**2)
+
+        tps_score = torch.exp(-1*torch.sum(detections.z *
+                              torch.sum(dist, dim=1)))
+
+        return tps_score
+
+    def fpc(self, detections: detections_loss, ground_truthes: detections_ground_truth):
+        """False Positive Class Loss
+        """
+        fpc_score = torch.tensor([0], dtype=torch.float,
+                                 device=detections.data.device)
+
+        gt_labels_for_nearest_dt = ground_truthes.class_labels[detections.nearest_gt_idx]
+        calculated_class_score = detections.class_scores.detach()
+
+        for i in range(calculated_class_score.shape[0]):
+            ignore_idx = gt_labels_for_nearest_dt[i]
+            calculated_class_score[i, ignore_idx] = 0
+
+            fpc_score[0] += (detections.r[i] *
+                             torch.log(torch.max(calculated_class_score[i])))
+        fpc_score *= -1
+        return fpc_score
+
+
 def tpc(detections: detections_loss, ground_truthes: detections_ground_truth):
     """True Positive Class Loss
     """
@@ -26,8 +92,6 @@ def tpc(detections: detections_loss, ground_truthes: detections_ground_truth):
 def tps(detections: detections_loss, ground_truthes: detections_ground_truth):
     """True Positive Shape Loss
     """
-    tps_score = torch.tensor([0], dtype=torch.float,
-                             device=detections.data.device)
 
     # (x-x)^2+(y-y)^2+(w-w)^2+(h-h)
     dist = (
@@ -51,8 +115,8 @@ def fpc(detections: detections_loss, ground_truthes: detections_ground_truth):
         ignore_idx = gt_labels_for_nearest_dt[i]
         calculated_class_score[i, ignore_idx] = 0
 
-        fpc_score += (detections.r[i] *
-                      torch.log(torch.max(calculated_class_score[i])))
+        fpc_score[0] += (detections.r[i] *
+                         torch.log(torch.max(calculated_class_score[i])))
     fpc_score *= -1
     return fpc_score
 
@@ -73,3 +137,80 @@ def total_loss(detections: detections_loss, ground_truthes: detections_ground_tr
 
     loss = tpc_score+tps_score+fpc_score
     return loss
+
+
+class TestNonObjectLoss(torch.nn.Module):
+
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, detections, ground_truthes: detections_ground_truth, gt_nearest_idx, z, r):
+        """Returns the total loss
+        Args:
+        detections:
+            yolo detections
+            (x1, y1, x2, y2, conf, cls)
+        ground_truthes:
+            ground truth detections
+            (x1, y1, x2, y2, conf, cls)
+        """
+        tpc_score = self.test_tpc(
+            detections, ground_truthes, z, gt_nearest_idx)
+        tps_score = self.test_tps(
+            detections, ground_truthes, z, gt_nearest_idx)
+        fpc_score = self.test_fpc(
+            detections, ground_truthes, r, gt_nearest_idx)
+
+        loss = tpc_score+tps_score+fpc_score
+        return loss
+
+    def test_tpc(self, detections, ground_truthes: detections_ground_truth, z, gt_nearest_idx):
+        """True Positive Class Loss
+        """
+        tpc_score = torch.tensor([0], dtype=torch.float,
+                                 device=detections.device)
+
+        gt_labels_for_nearest_dt = ground_truthes.class_labels[gt_nearest_idx]
+        calculated_class_score = detections[:, 5:].detach()
+
+        for i in range(calculated_class_score.shape[0]):
+            ignore_idx = gt_labels_for_nearest_dt[i]
+            calculated_class_score[i, ignore_idx] = 0
+
+            tpc_score[0] += (z[i] *
+                             torch.log(torch.max(calculated_class_score[i])))
+
+        tpc_score *= -1
+        return tpc_score
+
+    def test_tps(self, detections, ground_truthes: detections_ground_truth, z, gt_nearest_idx):
+        """True Positve Shape Loss
+        """
+        tps_score = torch.tensor([0], dtype=torch.float,
+                                 device=detections.device)
+
+        # (x-x)^2+(y-y)^2+(w-w)^2+(h-h)
+        dist = (
+            (detections[:, :4]-ground_truthes.xywh[gt_nearest_idx])**2)
+
+        tps_score = torch.exp(-1*torch.sum(z*torch.sum(dist, dim=1)))
+
+        return tps_score
+
+    def test_fpc(self, detections, ground_truthes: detections_ground_truth, r, gt_nearest_idx):
+        """False Positive Class Loss
+        """
+        fpc_score = torch.tensor([0], dtype=torch.float,
+                                 device=detections.device)
+
+        gt_labels_for_nearest_dt = ground_truthes.class_labels[gt_nearest_idx]
+        calculated_class_score = detections[:, 5:].detach()
+
+        for i in range(calculated_class_score.shape[0]):
+            ignore_idx = gt_labels_for_nearest_dt[i]
+            calculated_class_score[i, ignore_idx] = 0
+
+            fpc_score[0] += (r[i] *
+                             torch.log(torch.max(calculated_class_score[i])))
+        fpc_score *= -1
+        return fpc_score
--- SSD-background-patches/test.py
+++ SSD-background-patches/test.py
@@ -13,6 +13,7 @@ from box import condition, seek
 from model import yolo, yolo_util
 from dataset import coco
 from loss import total_loss
+from torchviz import make_dot
 import proposed_func as pf
 
 
@@ -69,6 +70,9 @@ def test_loss(orig_img):
     t = 0  # t in paper (iterator)
     psnr_threshold = 0
 
+    # 勾配計算失敗時のデバッグ用
+    torch.autograd.set_detect_anomaly(True)
+
     if torch.cuda.is_available():
         gpu_img = orig_img.to(
             device='cuda:0', dtype=torch.float)
@@ -76,15 +80,13 @@ def test_loss(orig_img):
 
     with torch.no_grad():
         # 素の画像を物体検出器にかけた時の出力をground truthとする
-        ground_truthes = yolo_util.detect(gpu_img)
-        ground_truthes = yolo_util.nms(ground_truthes)
-        ground_truthes = yolo_util.detections_ground_truth(ground_truthes[0])
+        gt_out = yolo_util.detect(gpu_img)
+        gt_mns = yolo_util.nms(gt_out)
+        ground_truthes = yolo_util.detections_ground_truth(gt_mns[0])
 
     n_b = 3  # 論文内で定められたパッチ生成枚数を指定するためのパラメータ
     background_patch_box = torch.zeros(
         [ground_truthes.total_det*n_b, 4], device=gpu_img.device)
-
-    optimizer = optim.Adam([gpu_img])
 
     model = yolo.load_model(
         "weights/yolov3.cfg",
@@ -97,38 +99,73 @@ def test_loss(orig_img):
 
         # t回目のパッチ適用画像から物体検出する
 
-        detections = model(gpu_img)
-        detections = yolo_util.detections_loss(detections[0])
+        output = model(gpu_img)
+        detections = yolo_util.nms(output)
+        detections_nms = yolo_util.detections_loss(detections[0])
 
-        # 検出と一番近いground truthのインデックス
+        # 検出と一番近いground truth
         gt_nearest_idx = seek.find_nearest_box(
-            detections.xywh, ground_truthes.xywh)
+            detections_nms.xywh, ground_truthes.xywh)
         gt_box_nearest_dt = ground_truthes.xyxy[gt_nearest_idx]
         # detectionと、各detectionに一番近いground truthのiouスコアを算出
         dt_gt_iou_scores = condition.iou(
-            detections.xyxy, gt_box_nearest_dt)
+            detections_nms.xyxy, gt_box_nearest_dt)
         # dtのスコアから、gt_nearest_idxで指定されるground truthの属するクラスのみを抽出
-        dt_scores_for_nearest_gt_label = detections.class_scores.gather(
+        dt_scores_for_nearest_gt_label = detections_nms.class_scores.gather(
             1, ground_truthes.class_labels[gt_nearest_idx, None]).squeeze()
         # 論文で提案された変数zを計算
         z = pf.calc_z(dt_scores_for_nearest_gt_label, dt_gt_iou_scores)
 
         # 検出と一番近い背景パッチ
         bp_nearest_idx = seek.find_nearest_box(
-            detections.xywh, background_patch_box)
+            detections_nms.xywh, background_patch_box)
         # detectionと、各detectionに一番近いground truthのiouスコアを算出
         bp_box_nearest_dt = background_patch_box[bp_nearest_idx]
         dt_bp_iou_scores = condition.iou(
-            detections.xyxy, bp_box_nearest_dt)
+            detections_nms.xyxy, bp_box_nearest_dt)
         # 論文で提案された変数rを計算
-        r = pf.calc_r(dt_bp_iou_scores, detections.xyxy, ground_truthes.xyxy)
+        r = pf.calc_r(dt_bp_iou_scores, detections_nms.xyxy,
+                      ground_truthes.xyxy)
 
         # 損失計算用の情報を積む
-        detections.set_loss_info(gt_nearest_idx, z, r)
+        detections_nms.set_loss_info(gt_nearest_idx, z, r)
 
-        loss = total_loss(detections, ground_truthes)
+        loss = total_loss(detections_nms, ground_truthes)
 
-        optimizer.zero_grad()
+        """
+        det_xyxy = condition.xywh2xyxy(detections[:, :4])
+
+        # 検出と一番近いground truthのインデックス
+        gt_nearest_idx = seek.find_nearest_box(
+            detections[:, :4], ground_truthes.xywh)
+        gt_box_nearest_dt = ground_truthes.xyxy[gt_nearest_idx]
+        # detectionと、各detectionに一番近いground truthのiouスコアを算出
+        dt_gt_iou_scores = condition.iou(
+            det_xyxy, gt_box_nearest_dt)
+        # dtのスコアから、gt_nearest_idxで指定されるground truthの属するクラスのみを抽出
+        dt_scores_for_nearest_gt_label = detections[:, 5:].gather(
+            1, ground_truthes.class_labels[gt_nearest_idx, None]).squeeze()
+        # 論文で提案された変数zを計算
+        z = pf.calc_z(dt_scores_for_nearest_gt_label, dt_gt_iou_scores)
+
+        # 検出と一番近い背景パッチ
+        bp_nearest_idx = seek.find_nearest_box(
+            detections[:, :4], background_patch_box)
+        # detectionと、各detectionに一番近いground truthのiouスコアを算出
+        bp_box_nearest_dt = background_patch_box[bp_nearest_idx]
+        dt_bp_iou_scores = condition.iou(
+            det_xyxy, bp_box_nearest_dt)
+        # 論文で提案された変数rを計算
+        r = pf.calc_r(dt_bp_iou_scores, det_xyxy, ground_truthes.xyxy)
+
+        # 損失計算用の情報を積む
+        # detections.set_loss_info(gt_nearest_idx, z, r)
+
+        test_total_loss = TestNonObjectLoss()
+        loss = test_total_loss(detections, ground_truthes,
+                               gt_nearest_idx, z, r)
+        """
+
         loss.backward()
 
         grad_img = gpu_img.grad
