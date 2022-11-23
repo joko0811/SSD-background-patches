4dd93dff5dd41e40e76b5036a5d1c7f72c6f71f2
joko0811 <okano@sec.inf.shizuoka.ac.jp>
2022-11-23 13:36:35 +0900

    lossをdetachなしで再実装

--- SSD-background-patches/loss.py
+++ SSD-background-patches/loss.py
@@ -3,89 +3,13 @@ import torch
 from model.yolo_util import detections_ground_truth, detections_loss
 
 
-class BPLoss(torch.nn.Module):
-    def __init__(self):
-        super(BPLoss, self).__init__()
-
-    def forward(self, detections: detections_loss, ground_truthes: detections_ground_truth):
-
-        tpc_score = self.tpc(detections, ground_truthes)
-        tps_score = self.tps(detections, ground_truthes)
-        fpc_score = self.fpc(detections, ground_truthes)
-
-        loss = tpc_score+tps_score+fpc_score
-        return loss
-
-    def tpc(self, detections: detections_loss, ground_truthes: detections_ground_truth):
-        """True Positive Class Loss
-        """
-        tpc_score = torch.tensor([0], dtype=torch.float,
-                                 device=detections.data.device)
-
-        gt_labels_for_nearest_dt = ground_truthes.class_labels[detections.nearest_gt_idx]
-        calculated_class_score = detections.class_scores.detach()
-
-        for i in range(calculated_class_score.shape[0]):
-            ignore_idx = gt_labels_for_nearest_dt[i]
-            calculated_class_score[i, ignore_idx] = 0
-
-            tpc_score[0] += (detections.z[i] *
-                             torch.log(torch.max(calculated_class_score[i])))
-
-        tpc_score *= -1
-        return tpc_score
-
-    def tps(self, detections: detections_loss, ground_truthes: detections_ground_truth):
-        """True Positive Shape Loss
-        """
-        tps_score = torch.tensor([0], dtype=torch.float,
-                                 device=detections.data.device)
-
-        # (x-x)^2+(y-y)^2+(w-w)^2+(h-h)
-        dist = (
-            (detections.xywh-ground_truthes.xywh[detections.nearest_gt_idx])**2)
-
-        tps_score = torch.exp(-1*torch.sum(detections.z *
-                              torch.sum(dist, dim=1)))
-
-        return tps_score
-
-    def fpc(self, detections: detections_loss, ground_truthes: detections_ground_truth):
-        """False Positive Class Loss
-        """
-        fpc_score = torch.tensor([0], dtype=torch.float,
-                                 device=detections.data.device)
-
-        gt_labels_for_nearest_dt = ground_truthes.class_labels[detections.nearest_gt_idx]
-        calculated_class_score = detections.class_scores.detach()
-
-        for i in range(calculated_class_score.shape[0]):
-            ignore_idx = gt_labels_for_nearest_dt[i]
-            calculated_class_score[i, ignore_idx] = 0
-
-            fpc_score[0] += (detections.r[i] *
-                             torch.log(torch.max(calculated_class_score[i])))
-        fpc_score *= -1
-        return fpc_score
-
-
-def tpc(detections: detections_loss, ground_truthes: detections_ground_truth):
+def tpc(detections: detections_loss, max_class_scores):
     """True Positive Class Loss
     """
-    tpc_score = torch.tensor([0], dtype=torch.float,
-                             device=detections.data.device)
 
-    gt_labels_for_nearest_dt = ground_truthes.class_labels[detections.nearest_gt_idx]
-    calculated_class_score = detections.class_scores.detach()
+    # スコア計算
+    tpc_score = -1 * torch.sum(detections.z * torch.log(max_class_scores))
 
-    for i in range(calculated_class_score.shape[0]):
-        ignore_idx = gt_labels_for_nearest_dt[i]
-        calculated_class_score[i, ignore_idx] = 0
-
-        tpc_score[0] += (detections.z[i] *
-                         torch.log(torch.max(calculated_class_score[i])))
-
-    tpc_score *= -1
     return tpc_score
 
 
@@ -102,22 +26,13 @@ def tps(detections: detections_loss, ground_truthes: detections_ground_truth):
     return tps_score
 
 
-def fpc(detections: detections_loss, ground_truthes: detections_ground_truth):
+def fpc(detections: detections_loss, max_class_scores):
     """False Positive Class Loss
     """
-    fpc_score = torch.tensor([0], dtype=torch.float,
-                             device=detections.data.device)
 
-    gt_labels_for_nearest_dt = ground_truthes.class_labels[detections.nearest_gt_idx]
-    calculated_class_score = detections.class_scores.detach()
+    # スコア計算
+    fpc_score = -1 * torch.sum(detections.r * torch.log(max_class_scores))
 
-    for i in range(calculated_class_score.shape[0]):
-        ignore_idx = gt_labels_for_nearest_dt[i]
-        calculated_class_score[i, ignore_idx] = 0
-
-        fpc_score[0] += (detections.r[i] *
-                         torch.log(torch.max(calculated_class_score[i])))
-    fpc_score *= -1
     return fpc_score
 
 
@@ -131,86 +46,36 @@ def total_loss(detections: detections_loss, ground_truthes: detections_ground_tr
         ground truth detections
         (x1, y1, x2, y2, conf, cls)
     """
-    tpc_score = tpc(detections, ground_truthes)
+
+    # tpc、fpcの計算に必要なパラメータ計算
+    max_class_scores = get_max_scores_without_correct_class(
+        detections, ground_truthes)
+
+    tpc_score = tpc(detections, max_class_scores)
     tps_score = tps(detections, ground_truthes)
-    fpc_score = fpc(detections, ground_truthes)
+    fpc_score = fpc(detections, max_class_scores)
 
     loss = tpc_score+tps_score+fpc_score
     return loss
 
 
-class TestNonObjectLoss(torch.nn.Module):
+def get_max_scores_without_correct_class(detections: detections_loss, ground_truthes: detections_ground_truth):
+    # detections.class_scoresで、計算対象外のクラスのクラススコアを0にしたtensorであるmasked_class_scoresを作成する
+    # 検出ごとに一番近いground truthのクラスラベルを抽出
+    gt_labels_for_nearest_dt = ground_truthes.class_labels[detections.nearest_gt_idx].unsqueeze(
+        dim=1)
+    # 次の行で参照するためだけのtensor
+    ccs_src = torch.zeros(
+        (detections.class_scores.shape[0], 1), device=detections.class_scores.device)
+    # t番目の検出で計算対象外となるクラスはgt_labels_for_nearest_dt[t]
+    # 計算対象外クラスが0、それ以外のクラスで1を取るマスク用配列
+    mask_for_class_score = torch.ones(
+        detections.class_scores.shape, device=detections.class_scores.device).scatter_(1, gt_labels_for_nearest_dt, ccs_src)
+    # 計算対象外のクラスのクラススコアを0にした配列
+    masked_class_scores = torch.mul(
+        mask_for_class_score, detections.class_scores)
 
-    def __init__(self):
-        super().__init__()
+    # 正しいクラス以外のすべての全クラスのなかで最大のスコアを抽出
+    max_class_scores = torch.max(masked_class_scores, dim=1)[0]
 
-    def forward(self, detections, ground_truthes: detections_ground_truth, gt_nearest_idx, z, r):
-        """Returns the total loss
-        Args:
-        detections:
-            yolo detections
-            (x1, y1, x2, y2, conf, cls)
-        ground_truthes:
-            ground truth detections
-            (x1, y1, x2, y2, conf, cls)
-        """
-        tpc_score = self.test_tpc(
-            detections, ground_truthes, z, gt_nearest_idx)
-        tps_score = self.test_tps(
-            detections, ground_truthes, z, gt_nearest_idx)
-        fpc_score = self.test_fpc(
-            detections, ground_truthes, r, gt_nearest_idx)
-
-        loss = tpc_score+tps_score+fpc_score
-        return loss
-
-    def test_tpc(self, detections, ground_truthes: detections_ground_truth, z, gt_nearest_idx):
-        """True Positive Class Loss
-        """
-        tpc_score = torch.tensor([0], dtype=torch.float,
-                                 device=detections.device)
-
-        gt_labels_for_nearest_dt = ground_truthes.class_labels[gt_nearest_idx]
-        calculated_class_score = detections[:, 5:].detach()
-
-        for i in range(calculated_class_score.shape[0]):
-            ignore_idx = gt_labels_for_nearest_dt[i]
-            calculated_class_score[i, ignore_idx] = 0
-
-            tpc_score[0] += (z[i] *
-                             torch.log(torch.max(calculated_class_score[i])))
-
-        tpc_score *= -1
-        return tpc_score
-
-    def test_tps(self, detections, ground_truthes: detections_ground_truth, z, gt_nearest_idx):
-        """True Positve Shape Loss
-        """
-        tps_score = torch.tensor([0], dtype=torch.float,
-                                 device=detections.device)
-
-        # (x-x)^2+(y-y)^2+(w-w)^2+(h-h)
-        dist = (
-            (detections[:, :4]-ground_truthes.xywh[gt_nearest_idx])**2)
-
-        tps_score = torch.exp(-1*torch.sum(z*torch.sum(dist, dim=1)))
-
-        return tps_score
-
-    def test_fpc(self, detections, ground_truthes: detections_ground_truth, r, gt_nearest_idx):
-        """False Positive Class Loss
-        """
-        fpc_score = torch.tensor([0], dtype=torch.float,
-                                 device=detections.device)
-
-        gt_labels_for_nearest_dt = ground_truthes.class_labels[gt_nearest_idx]
-        calculated_class_score = detections[:, 5:].detach()
-
-        for i in range(calculated_class_score.shape[0]):
-            ignore_idx = gt_labels_for_nearest_dt[i]
-            calculated_class_score[i, ignore_idx] = 0
-
-            fpc_score[0] += (r[i] *
-                             torch.log(torch.max(calculated_class_score[i])))
-        fpc_score *= -1
-        return fpc_score
+    return max_class_scores
