16cf8a7656c0ba69780a566b2e904370d645f358
joko0811 <okano@sec.inf.shizuoka.ac.jp>
2022-11-28 20:21:48 +0900

    評価時に用いるデータセットの作成

---
+++ SSD-background-patches/dataset/simple.py
@@ -0,0 +1,21 @@
+import glob
+from PIL import Image
+from torch.utils.data import Dataset
+
+
+class DirectoryDataset(Dataset):
+    def __init__(self, image_path, transform=None):
+        self.image_path = image_path
+        self.files = sorted(glob.glob("%s/*.*" % self.image_path))
+        self.transform = transform
+
+    def __getitem__(self, index):
+        image_path = self.files[index % len(self.files)]
+        image = Image.open(image_path)
+
+        if self.transform is not None:
+            image = self.transform(image)
+        return image
+
+    def __len__(self):
+        return len(self.files)
