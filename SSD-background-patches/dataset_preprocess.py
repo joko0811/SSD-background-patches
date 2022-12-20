d4c604b43496b1fde73709daae70dcb90e4be3ff
joko0811 <okano@sec.inf.shizuoka.ac.jp>
2022-12-20 09:22:09 +0900

    preprocess 作成中断

--- SSD-background-patches/dataset_preprocess.py
+++ SSD-background-patches/dataset_preprocess.py
@@ -1,3 +1,9 @@
+"""
+作るのやめた
+"""
+import os
+import time
+
 import numpy as np
 
 from tqdm import tqdm
@@ -14,7 +20,7 @@ from box import condition
 
 
 def is_extractable_patch_for_image(detections, image):
-
+    image_extractable = True
     for i in range(detections.total_group):
 
         group_xyxy = detections.xyxy[detections.group_labels == i]
@@ -23,33 +29,35 @@ def is_extractable_patch_for_image(detections, image):
         # 探索領域決定
         search_image, search_area = pf.calculate_search_area(
             image, group_xyxy)
-        is_extractable_patch_for_group()
+        group_extractable = is_extractable_patch_for_group(
+            search_area[:2], list(search_image.shape[2:]), group_xywh, group_xyxy)
+
+        image_extractable = image_extractable and group_extractable
 
         # 最終的にグループ毎に3つ領域が選択できていたらサブセットに含める
-    return
-    # TODO:パッチ初期化時にパッチ領域を選択できる（＝十分な背景領域が存在する）画像を抽出するプログラムを書く
+    return image_extractable
 
 
-def is_extractable_patch_for_group(image, ignore_boxes, search_area):
+def is_extractable_patch_for_group(x1y1_partial_image, hw_partial_image, group_xywh, ignore_boxes):
     """
     """
     n_b = 3
 
     # ウインドウの高さ、幅の決定
-    window_list_w, window_list_h = pf.calculate_window_wh()  # TODO
+    window_list_w, window_list_h = pf.calculate_window_wh(group_xywh)
 
     extractable_patch_num = 0
 
     for window_w, window_h in zip(window_list_w, window_list_h):
-        window_box_map = pf.create_box_map_of_original_image()  # TODO
+        window_box_map = pf.create_box_map_of_original_image(
+            x1y1_partial_image, hw_partial_image, window_w, window_h)
         # [h,w,4]->[w*h,4]
         window_box_list = window_box_map.reshape(
             (window_box_map.shape[0]*window_box_map.shape[1], 4))
         overlap_table = condition.are_overlap_list(
             window_box_list, ignore_boxes)
 
-        extractable_patch_num += overlap_table.nonzero(
-        ).shape[0]
+        extractable_patch_num += overlap_table.nonzero().shape[0]
 
         if extractable_patch_num >= n_b:
             break
@@ -59,6 +67,25 @@ def is_extractable_patch_for_group(image, ignore_boxes, search_area):
         return False
 
     return True
+
+
+def make_sabset_dir():
+    time_str = time.strftime("%Y%m%d_%H%M%S")
+    subset_path = "./subset_"+time_str
+    subset_image_dir_path = subset_path+"images/"
+    subset_label_dir_path = subset_path+"labels/"
+
+    os.mkdir(subset_path)
+    os.mkdir(subset_image_dir_path)
+    os.mkdir(subset_label_dir_path)
+
+    return
+
+
+def save_sabset_info(subset_path, image, ground_truth):
+    image_dir_name = subset_path+"images/"
+    label_dir_name = subset_path+"labels/"
+    return
 
 
 def gen_subset_with_sufficient_background_area(dataloader, model):
@@ -87,6 +114,9 @@ def gen_subset_with_sufficient_background_area(dataloader, model):
             detections.xywh.detach().cpu().numpy())
         detections.set_group_info(torch.from_numpy(
             group_labels.astype(np.float32)).to(device))
+
+        if is_extractable_patch_for_image(detections, gt_image):
+            save_subset_info()
 
     return
 
