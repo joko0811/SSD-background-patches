344d7f65947cc357f3bb4bc083540058366b4bd6
joko0811 <okano@sec.inf.shizuoka.ac.jp>
2023-01-17 21:21:39 +0900

    add gitignore

--- ssd-background-patches/test.py
+++ ssd-background-patches/test.py
@@ -1,14 +1,3 @@
-import numpy as np
-import cv2
-
-import torch
-from torchvision.datasets.coco import CocoDetection
-from torchvision import transforms
-
-from pytorchyolo.utils.transforms import Resize, DEFAULT_TRANSFORMS
-
-
-from util import img
 
 
 def main():
