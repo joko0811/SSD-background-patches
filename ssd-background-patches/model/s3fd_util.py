6cbb6fc0c55b30c2161ddfa2eaaf4d22a0a54364
joko0811 <okano@sec.inf.shizuoka.ac.jp>
2023-03-15 14:14:12 +0900

    fix s3fd transform

--- ssd-background-patches/model/s3fd_util.py
+++ ssd-background-patches/model/s3fd_util.py
@@ -2,19 +2,30 @@ import numpy as np
 
 import torch
 
-import cv2
-from PIL import Image
 from torchvision import transforms
 
 from model.S3FD import s3fd
 from box.boxio import detections_base
 
-from typing import Tuple
+
+class S3fdResize:
+    def __init__(self, scale_reference_size=(1700, 1200)):
+        self.scale_reference = scale_reference_size[0]*scale_reference_size[1]
+
+    def __call__(self, pic):
+        width, height = transforms.functional.get_image_size(pic)
+        x_area = width*height
+
+        scale = np.sqrt(self.scale_reference/x_area)
+        out_size = (round(height*scale), round(width*scale))
+        return transforms.functional.resize(img=pic, size=out_size)
+
 
 S3FD_TRANSFORMS = transforms.Compose([
-    transforms.Resize((1700, 1200)),
+    S3fdResize(),
     transforms.PILToTensor(),
 ])
+
 
 # caffe model RGB nomalization?
 S3FD_MASIC_NUMBER = torch.tensor([123., 117., 104.], dtype=torch.float)
@@ -34,97 +45,6 @@ def load_model(weight_path):
     model.load_state_dict(torch.load(weight_path))
 
     return model.to(device)
-
-
-def image_list_encode(pil_image_list: Image.Image, is_mask=False) -> Tuple[torch.Tensor, torch.Tensor]:
-    """image_encode wrap supporting list encoding.
-    Args:
-        pil_image_list: list of pil image
-    Return:
-        tensor_image: Tensor list of s3fd input representation
-    """
-    setuped_image_list = list()
-    scale_list = list()
-    for pil_image in pil_image_list:
-        image, scale = image_encode(pil_image, is_mask)
-        setuped_image_list.append(image)
-        scale_list.append(scale)
-
-    return torch.cat(setuped_image_list).contiguous(), torch.cat(scale_list).contiguous()
-
-
-def image_encode(pil_image, is_mask=False):
-    """I don't understand pre-processing.
-    Args:
-        pil_image: a pil image
-    Return:
-        tensor_image: Tensor of s3fd input representation
-    """
-
-    # pil to ndarray
-    if pil_image.mode == 'L':
-        # 8-bit pixels, black and white
-        pil_image = pil_image.convert('RGB')
-    np_image = np.array(pil_image)
-
-    # resize
-    height, width, _ = np_image.shape
-    max_im_shrink = np.sqrt(
-        1700 * 1200 / (height * width))
-    resize_image = cv2.resize(np_image, None, None, fx=max_im_shrink,
-                              fy=max_im_shrink, interpolation=cv2.INTER_LINEAR)
-
-    # HWC to CHW
-    chw_image = hwc2chw(resize_image).astype('float32')
-
-    if not is_mask:
-        # nomalization
-        nomalized_image = chw_image-S3FD_NOMALIZED_ARRAY
-    else:
-        nomalized_image = chw_image
-
-    # numpy to tensor
-    tensor_image = torch.from_numpy(nomalized_image).unsqueeze(0)
-
-    # old width height array
-    scale = torch.Tensor([width, height,
-                          width, height]).unsqueeze(0)
-
-    return tensor_image, scale
-
-
-def image_decode(tensor_image: torch.Tensor, scale=None):
-    """
-    Args:
-        tensor_image: Tensor of s3fd input representation
-        scale: image_encode() output
-    Return:
-        image: pil image
-    """
-    if scale is None:
-        scale = torch.tensor([1700, 1200])
-
-    np_image = tensor_image.detach().cpu().resolve_conj().resolve_neg().numpy()
-    before_nomalized_image = np_image+S3FD_NOMALIZED_ARRAY
-
-    # CHW to HWC
-    hwc_image = chw2hwc(before_nomalized_image)
-
-    max_im_shrink = float(
-        1/np.sqrt(1700*1200/(scale[0].item() * scale[1].item())))
-    resize_image = cv2.resize(hwc_image, None, None, fx=max_im_shrink,
-                              fy=max_im_shrink, interpolation=cv2.INTER_LINEAR).astype('uint8')
-    pil_image = Image.fromarray(resize_image)
-
-    return pil_image
-
-
-def hwc2chw(np_image):
-    return np.swapaxes(np.swapaxes(np_image, 1, 2), 0, 1)
-
-
-def chw2hwc(np_image):
-    return np.swapaxes(np.swapaxes(np_image, 0, 1), 1, 2)
 
 
 def make_detections_list(data_list, scale_list, detection_class, thresh):
