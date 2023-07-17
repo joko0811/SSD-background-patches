38555b64cc247c0cd15dd35b346141946c85add3
joko0811 <okano@sec.inf.shizuoka.ac.jp>
2023-07-17 14:33:47 +0900

    add function for calc face area

--- ssd-background-patches/eval_image.py
+++ ssd-background-patches/eval_image.py
@@ -1,49 +1,26 @@
 import os
+from tqdm import tqdm
+import pandas as pd
 
 import torch
 from torchvision import transforms
 
-from tqdm import tqdm
 
 from model.S3FD import s3fd
 from model.s3fd_util import S3fdResize
 from dataset.simple import DirectoryImageDataset
 
 
-def generate_data(path):
-    def _load_model(path):
-        torch.set_default_tensor_type("torch.cuda.FloatTensor")
-
-        # Select device for inference
-        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-        model = s3fd.build_s3fd("test", 2)
-        model.load_state_dict(torch.load(path))
-        return model.to(device)
-
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    BATCH_SIZE = 4
-
+def generate_data(model, image_set, path):
     out_path = os.path.join(path, "detection/")
-    image_dir_path = os.path.join(path, "face/")
 
     if not os.path.exists(out_path):
         os.makedirs(out_path)
 
-    S3FD_TRANSFORMS = transforms.Compose(
-        [
-            S3fdResize(),
-            transforms.PILToTensor(),
-            # transforms.ConvertImageDtype(torch.float),
-        ]
-    )
-
-    image_set = DirectoryImageDataset(image_dir_path, transform=S3FD_TRANSFORMS)
     image_loader = torch.utils.data.DataLoader(image_set, batch_size=BATCH_SIZE)
 
-    model = _load_model("weights/s3fd.pth")
-    model.eval()
-
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    BATCH_SIZE = 1
     thresh = 0.6
     for image, image_path in tqdm(image_loader):
         image = image.to(device=device, dtype=torch.float)
@@ -82,13 +59,81 @@ def generate_data(path):
     print("complete!")
 
 
+def calculate_face_area(model, image_set):
+    """
+    全部の顔画像について顔領域の面積計算を行い、保存する
+    例えばタイルと顔領域の顔領域比を考慮するために用いる
+    """
+    BATCH_SIZE = 1
+    image_loader = torch.utils.data.DataLoader(image_set, batch_size=BATCH_SIZE)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    thresh = 0.6
+
+    area_df_columns = ["area"]
+    area_df = pd.DataFrame([], columns=area_df_columns)
+    for image_list, image_path_list in tqdm(image_loader):
+        image_list = image_list.to(device=device, dtype=torch.float)
+        output = model(image_list)
+
+        for image_idx, det in enumerate(output):
+            extract_output = det[det[..., 0] >= thresh]
+            for det in extract_output:
+                if det.nelement() == 0:
+                    continue
+                image_height, image_width = image_list[image_idx].shape[1:]
+                det_height = det[..., 3] - det[..., 1]
+                det_width = det[..., 2] - det[..., 0]
+
+                # 出力された座標は画像サイズで正規化されているため乗算
+                area = (
+                    ((image_height * det_height) * (image_width * det_width))
+                    .detach()
+                    .cpu()
+                    .resolve_conj()
+                    .resolve_neg()
+                    .numpy()
+                )
+                area_df.loc[image_path_list[image_idx]] = area
+
+    area_df.to_csv("./all_face_area.csv")
+
+
 def main():
-    # path = sys.argv[1]
-    path = "datasets/casia/train/"
+    # データセットの画像全部に対して処理をする
+    path = "datasets/faceforensics_pp/train"
+    image_dir_path = os.path.join(path, "face/")
+
+    def _load_model(path):
+        torch.set_default_tensor_type("torch.cuda.FloatTensor")
+
+        # Select device for inference
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+        model = s3fd.build_s3fd("test", 2)
+        model.load_state_dict(torch.load(path))
+        return model.to(device)
+
+    S3FD_TRANSFORMS = transforms.Compose(
+        [
+            S3fdResize(),
+            transforms.PILToTensor(),
+            # transforms.ConvertImageDtype(torch.float),
+        ]
+    )
+
+    image_set = DirectoryImageDataset(image_dir_path, transform=S3FD_TRANSFORMS)
+
+    model = _load_model("weights/s3fd.pth")
+    model.eval()
 
     with torch.no_grad():
-        generate_data(path)
-    # tbx_monitor(path)
+        # 画像を読み込んで画像と同名の検出ファイルを作成する(yolo形式)
+        # generate_data(model, image_set, path)
+
+        # データセットの画像を全探索して全部の面積を算出する
+        # 分布を見たい時にやる
+        calculate_face_area(model, image_set)
 
 
 if __name__ == "__main__":
