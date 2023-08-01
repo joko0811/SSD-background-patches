6a63bf5c9c42092b9f34f9186bc3d373b48f2541
joko0811 <okano@sec.inf.shizuoka.ac.jp>
2023-08-01 13:57:53 +0900

    split function

--- ssd-background-patches/detection/tp_fp_manager.py
+++ ssd-background-patches/detection/tp_fp_manager.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+import torch
 from detection.detection_base import DetectionsBase
 from evaluation.detection import list_iou
 
@@ -23,7 +24,7 @@ class TpFpManager:
         ground_truth: DetectionsBase,
         iou_thresh=0.5,
     ):
-        """
+        """評価指標算出のための途中経過を記録する関数
         提案される顔領域を追加する
         同時にTP、FPなどの加算も行う
         Args:
@@ -31,55 +32,111 @@ class TpFpManager:
             ground_truth: 追加する検出に対応する真の顔領域。初期化段階で登録していない時に指定する
             iou_thresh: TP,FPの判定基準となるIoU閾値
         """
-        if (ground_truth is None) and (detection is None):
-            return
+        gt_det, tp_det, fp_det, fn_det = self.judge_detection(
+            detection, ground_truth, iou_thresh
+        )
+        det_conf_array, det_tp_binary_array = self.calc_sklearn_y_true_score(
+            detection, ground_truth, iou_thresh
+        )
 
-        elif (ground_truth is None) and (detection is not None):
-            self.det_tp_binary_array = np.append(
-                self.det_tp_binary_array, np.zeros(len(detection))
-            )
-            self.det_conf_array = np.append(
-                self.det_conf_array,
-                detection.conf.detach().cpu().resolve_conj().resolve_neg().numpy(),
-            )
-            self.fp += len(detection)
-            return
+        self.gt += len(gt_det)
+        self.tp += len(tp_det)
+        self.fp += len(fp_det)
+        self.fn += len(fn_det)
+
+        self.det_conf_array = np.append(self.det_conf_array, det_conf_array)
+        self.det_tp_binary_array = np.append(
+            self.det_tp_binary_array, det_tp_binary_array
+        )
+
+    def judge_detection(
+        self,
+        detection: DetectionsBase,
+        ground_truth: DetectionsBase,
+        iou_thresh=0.5,
+    ):
+        gt_det = torch.tensor([]).to(device=detection.conf.device)
+        tp_det = torch.tensor([]).to(device=detection.conf.device)
+        fp_det = torch.tensor([]).to(device=detection.conf.device)
+        fn_det = torch.tensor([]).to(device=detection.conf.device)
+
+        if (ground_truth is None) and (detection is not None):
+            fp_det = torch.cat((detection.conf.unsqueeze(1), detection.xyxy), dim=1)
 
         elif (ground_truth is not None) and (detection is None):
-            self.gt += len(ground_truth)
-            self.fn += len(ground_truth)
-            return
+            gt_det = torch.cat(
+                (ground_truth.conf.unsqueeze(1), ground_truth.xyxy), dim=1
+            )
+            fn_det = torch.cat((detection.conf.unsqueeze(1), detection.xyxy), dim=1)
 
         elif (ground_truth is not None) and (detection is not None):
-            self.gt += len(ground_truth)
+            gt_det = torch.cat(
+                (ground_truth.conf.unsqueeze(1), ground_truth.xyxy), dim=1
+            )
 
+            dt_gt_iou = list_iou(detection.xyxy, ground_truth.xyxy)
+
+            det_tp_binary = (dt_gt_iou >= iou_thresh).any(dim=1)
+            det_fp_binary = (dt_gt_iou < iou_thresh).all(dim=1)
+            det_fn_binary = (dt_gt_iou < iou_thresh).all(dim=0)
+
+            # 重なる検出が一つもない真の領域数を集計
+            # all(dim=0)で真の領域毎の重なる領域が一つもない場合Trueを取るboolに変換
+            # detach以降はtensor→intの変換処理
+            tp_det = torch.cat(
+                (
+                    detection.conf[det_tp_binary].unsqueeze(1),
+                    detection.xyxy[det_tp_binary],
+                ),
+                dim=1,
+            )
+            fp_det = torch.cat(
+                (
+                    detection.conf[det_fp_binary].unsqueeze(1),
+                    detection.xyxy[det_fp_binary],
+                ),
+                dim=1,
+            )
+            fn_det = torch.cat(
+                (
+                    ground_truth.conf[det_fn_binary].unsqueeze(1),
+                    ground_truth.xyxy[det_fn_binary],
+                ),
+                dim=1,
+            )
+
+        return gt_det, tp_det, fp_det, fn_det
+
+    def calc_sklearn_y_true_score(self, detection, ground_truth, iou_thresh=0.5):
+        det_conf = np.array([])
+        det_tp_binary = np.array([])
+
+        if (ground_truth is not None) and (detection is not None):
             dt_gt_iou = list_iou(detection.xyxy, ground_truth.xyxy)
 
             det_tp_binary = (
                 (dt_gt_iou >= iou_thresh)
                 .any(dim=1)
                 .long()
+                .detach()
+                .cpu()
+                .resolve_conj()
+                .resolve_neg()
+                .numpy()
             )
 
-            # 重なる検出が一つもない真の領域数を集計
-            # all(dim=0)で真の領域毎の重なる領域が一つもない場合Trueを取るboolに変換
-            # detach以降はtensor→intの変換処理
-            self.fn += ((dt_gt_iou < iou_thresh).all(dim=0).long().sum()
-                        ).detach().cpu().resolve_conj().resolve_neg().numpy().item()
-
-            self.tp += det_tp_binary.nonzero().shape[0]
-            self.fp += det_tp_binary.shape[0] - \
-                det_tp_binary.nonzero().shape[0]
-
-            self.det_tp_binary_array = np.append(
-                self.det_tp_binary_array,
-                det_tp_binary.detach().cpu().resolve_conj().resolve_neg().numpy(),
+            det_conf = (
+                detection.conf.detach().cpu().resolve_conj().resolve_neg().numpy()
             )
-            self.det_conf_array = np.append(
-                self.det_conf_array,
-                detection.conf.detach().cpu().resolve_conj().resolve_neg().numpy(),
+
+        elif detection is not None:
+            det_tp_binary = np.zeros(len(detection))
+
+            det_conf = (
+                detection.conf.detach().cpu().resolve_conj().resolve_neg().numpy()
             )
-            return
+
+        return det_conf, det_tp_binary
 
     def get_value(self):
         return (self.tp, self.fp, self.fn, self.gt)
