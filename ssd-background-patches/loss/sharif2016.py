4233341721ea3f402acf1d0165c337cd5228d6af
joko0811 <okano@sec.inf.shizuoka.ac.jp>
2024-01-26 15:21:02 +0900

    fix tv loss

--- ssd-background-patches/loss/sharif2016.py
+++ ssd-background-patches/loss/sharif2016.py
@@ -5,7 +5,7 @@ def tv_loss(image_list, gradation_strength=0.1):
     """
     Args:
         image_list: N*C*H*W
-        gradation_strength: The closer the value is to 1, the more monochromatic the patch, and the closer it is to 0, the more colorful the patch.
+        gradation_strength: The gradient of the patch becomes weaker the closer this value is to 0 and stronger the closer it is to 1.
     """
 
     roll_h_image = torch.roll(input=image_list, shifts=(0, 0, 1, 0), dims=(0, 1, 2, 3))
@@ -14,13 +14,11 @@ def tv_loss(image_list, gradation_strength=0.1):
     roll_w_image = torch.roll(input=image_list, shifts=(0, 0, 0, 1), dims=(0, 1, 2, 3))
     roll_w_image[:, :, :, 0] = 0
 
-    image_h_diff = torch.abs(image_list - roll_h_image)
-    image_w_diff = torch.abs(image_list - roll_w_image)
-    tv_score = (
-        torch.sum(torch.sqrt(torch.pow(image_h_diff, 2) + torch.pow(image_w_diff, 2)))
-        / gradation_strength
-        * (image_list.shape[2] * image_list.shape[3])
-    )
+    image_h_diff = image_list - roll_h_image
+    image_w_diff = image_list - roll_w_image
+    tv_score = torch.sum(
+        torch.sqrt(torch.pow(image_h_diff, 2) + torch.pow(image_w_diff, 2))
+    ) / ((1 - gradation_strength + 1e-9) * (image_list.shape[2] * image_list.shape[3]))
 
     return tv_score
 
