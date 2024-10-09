29b2fa437ebcb7adadff44f99e111dbc05fa37e4
joko0811 <okano@sec.inf.shizuoka.ac.jp>
2024-10-09 14:27:11 +0900

    add error message

--- ssd-background-patches/util/infoutil.py
+++ ssd-background-patches/util/infoutil.py
@@ -2,6 +2,10 @@ import git
 
 
 def get_git_sha():
-    repo = git.Repo(search_parent_directories=True)
-    sha = repo.head.object.hexsha
-    return sha
+    try:
+        repo = git.Repo(search_parent_directories=True)
+        sha = repo.head.object.hexsha
+        return sha
+    except Exception as e:
+        return "In this environment it was not possible to get the git commit hash."
+
